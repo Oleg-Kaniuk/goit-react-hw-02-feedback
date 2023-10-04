c70c38ad513b65a31195622705a7b0a5751b1d6c
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-10-04 12:35:44 +0300

    feedbackOptions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { MainContainer } from "./App.styled";
+// import { Section } from "../Section/Section";
+import { FeedbackOptions } from "../FeedbackOptions/FeedbackOptions";
+// import { Statistics } from "../Statistics/Statistics";
+// import { Notification } from "../Notification/Notification";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({ [state]: prevState[state] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100) || 0;
+  };
+
+   render() {
+    
+    const options = Object.keys(this.state);
+    
+    return (
+      <MainContainer>
+        
+          <FeedbackOptions
+            onLeaveFeedback={this.onLeaveFeedback}
+            options={options}
+          />
+        
+      </MainContainer>
+    );
+  }
+}
